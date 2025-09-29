a40a76b57802c330d79431e73de5e9e6303f6d46
yaakovsm <yaakovsm@gmail.com>
2025-09-29 18:41:48 +0300

    docker files changing

--- Backend/init-db.sql
+++ Backend/init-db.sql
@@ -1,24 +1,15 @@
 -- Database initialization script for PakalSpot
--- This script creates the database user and grants necessary permissions
+-- This script sets up PostGIS extensions and grants necessary permissions
 
--- Create the user if it doesn't exist
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'pakalspot_user') THEN
-        CREATE USER pakalspot_user WITH PASSWORD 'jcoffeebrew';
-    END IF;
-END
-$$;
+-- Enable PostGIS extension
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
 
--- Create the database if it doesn't exist
-SELECT 'CREATE DATABASE pakalspot_db OWNER pakalspot_user'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pakalspot_db')\gexec
+-- Grant all privileges on the current database to the user
+GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
 
--- Grant all privileges on the database to the user
-GRANT ALL PRIVILEGES ON DATABASE pakalspot_db TO pakalspot_user;
-
--- Connect to the database and grant schema privileges
-\c pakalspot_db;
-GRANT ALL ON SCHEMA public TO pakalspot_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO pakalspot_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO pakalspot_user;
+-- Grant schema privileges
+GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $POSTGRES_USER;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_USER;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO $POSTGRES_USER;
